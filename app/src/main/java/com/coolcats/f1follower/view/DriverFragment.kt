400e79bedf19380a9f5ac44055c34a8ce7c00f07
mlestes <murray.l.estes@gmail.com>
2021-05-11 23:00:07 -0400

    Add more info to Driver fragment

--- app/src/main/java/com/coolcats/f1follower/view/DriverFragment.kt
+++ app/src/main/java/com/coolcats/f1follower/view/DriverFragment.kt
@@ -29,8 +29,21 @@ class DriverFragment(private val result: Result) : Fragment() {
         team_text.text = result.Constructor.name
         nationality_text.text = result.Driver.nationality
         dob_text.text = result.Driver.dateOfBirth
-
-
+        if(result.FastestLap == null){
+            fast_lap_text.text = "N/A"
+            fast_info_text.text = "N/A"
+        }
+        else {
+            fast_lap_text.text = result.FastestLap.Time.time
+            fast_info_text.text = result.FastestLap.lap
+        }
+        position_info_text.text = result.position
+        if(result.Time == null) {
+            total_time_label.text = "Status:"
+            total_time_text.text = result.status
+        }
+        else
+            total_time_text.text = result.Time.time
     }
 
 }
